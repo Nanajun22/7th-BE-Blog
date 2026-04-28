a40adbddf49f2a93407305c4e197ab0513fb2002
Nanajun22 <skaudwns3@gmail.com>
2026-04-28 17:09:02 +0900

    제약조건 추가

--- src/main/java/com/example/leets7th/domain/report/dto/ReportRequestDto.java
+++ src/main/java/com/example/leets7th/domain/report/dto/ReportRequestDto.java
@@ -2,9 +2,22 @@ package com.example.leets7th.domain.report.dto;
 
 import com.example.leets7th.domain.report.domain.ReportContentType;
 import com.example.leets7th.domain.report.domain.ReportStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class ReportRequestDto {
-    public record ReportCreateReq(String reason,ReportContentType contentType,Long contentId) {}
+    public record ReportCreateReq(
+            @NotBlank(message = "신고 사유를 기재해주세요.")
+            String reason,
+
+            @NotNull(message = "컨텐츠 타입 미입력")
+            ReportContentType contentType,
+
+            @NotNull(message = "컨텐츠 아이디 미입력")
+            Long contentId
+    ) {
+
+    }
 
 
 }
